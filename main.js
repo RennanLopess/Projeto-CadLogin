b09cb803e7e3473dc014e702a85d7c1eb88b17c0
Rennan Rentz <rennanrentz@gmail.com>
2025-04-11 21:49:23 -0300

    Criando função para exibir nomes Cadastrados na lista d 'Nomes'

--- main.js
+++ main.js
@@ -19,9 +19,18 @@ function salvarUser(){
     
     if(nomeUser){
         dadosLista.push(nomeUser);
-        console.log(dadosLista);
+        criaLista();
         document.getElementById('nomeUser').value = "";
     }else{
         alert("Favor preencher com um nome");
+    }
+}
+
+// função para criar lista de nomes
+function criaLista(){
+    let tabela = "<tr><th>Nome</th><th>Ações</th></tr>";
+    for(let i = 0; i<= (dadosLista.length - 1);i++){
+        tabela += "<tr><td>" +dadosLista[i] + "</td><td></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
     }
 }
