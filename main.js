b7d37ac07623ea9f7a572ae26af255be06901715
Rennan Rentz <rennanrentz@gmail.com>
2025-04-11 22:12:05 -0300

    Dando ação para o botão excluir

--- main.js
+++ main.js
@@ -30,12 +30,13 @@ function salvarUser(){
 function criaLista(){
     let tabela = "<tr><th>Nome</th><th>Ações</th></tr>";
     for(let i = 0; i<= (dadosLista.length - 1);i++){
-        tabela += "<tr><td>" +dadosLista[i] + "</td><td><button class='btn btn-success'>Editar</button><button class='btn btn-danger'>Excluir</button></td></tr>";
+        tabela += "<tr><td>" +dadosLista[i] + "</td><td><button class='btn btn-success'>Editar</button><button class='btn btn-danger' onclick= excluir(this.parentNode.parentNode.rowIndex)>Excluir</button></td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
     }
 }
 
 // função para excluir nome da lista
 function excluir(i){
-    
+    dadosLista.splice((i - 1),1);
+    document.getElementById('tabela').deleteRow(i);
 }
