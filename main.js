672d39bb52c89875806eedf74162ee55da783994
Rennan Rentz <rennanrentz@gmail.com>
2025-04-11 21:33:39 -0300

    Inserindo botão de Salvar um nome na página de 'Cadastro de Usuário'

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 // validação de preenchimento de login
 function acessar() {
-    let loginEmail = document.getElementById('loginEmail').value
-    let loginSenha = document.getElementById('loginSenha').value
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginSenha = document.getElementById('loginSenha').value;
 
     // Verifica se os campos foram preenchidos
-    if (!loginEmail || loginSenha) {
+    if (!loginEmail || !loginSenha) {
         alert("Favor preencher todos os campos de acesso");
     }else{
-        
+        window.location.href = 'cadastro.html';
     }
+}
 
+// Declara array para o projeto
+var dadosLista = [];
+// Função de armazenamento de nomes em array
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+    
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+        console.log(dadosLista);
+        document.getElementById('nomeUser').value = "";
+    }else{
+        alert("Favor preencher com um nome");
+    }
 }
